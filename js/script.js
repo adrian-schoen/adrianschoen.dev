0405b8681a9ff6aa148138eed889b3b7f316d97f
Adrian Schön <schoenadrian@outlook.de>
2024-08-25 22:54:01 +0200

    Minimal index welcome design

--- js/script.js
+++ js/script.js
@@ -1,7 +1,10 @@
 $(document).ready(function() {
-    $('#scrollDown').click(function() {
-        const headerHeight = $('#header').outerHeight();
-        const $scrollTo = $('main');
+    const headerHeight = $('#header').outerHeight();
+    const $scrollTo = $('main');
+    let hasScrolled = false;
+
+    function scrollToMain() {
+        hasScrolled = true; // Set the flag to true
 
         $('html, body').animate({
             scrollTop: $scrollTo.offset().top - headerHeight
@@ -9,5 +12,17 @@ $(document).ready(function() {
 
         $('#header').addClass('fading-header visible');
         $('footer').addClass('fading-footer visible');
+
+        // Remove the scroll wheel event listener after the animation is triggered
+        $(window).off('wheel');
+    }
+
+    $('#scrollDown').click(function() {
+        scrollToMain();
+    });
+
+    // Can trigger only on first scroll, then listener removed
+    $(window).on('wheel', function(event) {
+        scrollToMain();
     });
 });
