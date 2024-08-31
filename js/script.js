b2f91f14073cd2667848229df9b7ad9ef8019646
Adrian Schön <schoenadrian@outlook.de>
2024-08-31 18:31:30 +0200

    cleaned up index.html and moved scripts to script.js

--- js/script.js
+++ js/script.js
@@ -10,24 +10,19 @@ function scrollToContainer(containerId) {
 }
 
 $(document).ready(function() {
-    // Check if the current page is index.html
     const currentPage = window.location.href.split('/').pop();
-    if (currentPage === '' || currentPage === 'index.html') { // on cloudflare deployment its adrianschoen.dev/, on local its .../index.html
+    if (currentPage === '' || currentPage === 'index.html') {
         const headerHeight = $('#header').outerHeight();
         const $scrollTo = $('main');
         let hasScrolled = false;
 
         function scrollToMain() {
-            hasScrolled = true; // Set the flag to true
-
+            hasScrolled = true;
             $('html, body').animate({
                 scrollTop: $scrollTo.offset().top - 100
             }, 1000); 
-
             $('#header').addClass('fading-header visible');
             $('footer').addClass('fading-footer visible');
-
-            // Remove the scroll wheel event listener after the animation is triggered
             $(window).off('wheel');
         }
 
@@ -43,13 +38,11 @@ $(document).ready(function() {
             scrollToMain();
         });
 
-        // Show header and footer immediately on first scroll
         $(window).on('wheel', function(event) {
             showHeaderFooter();
         });
     }
 
-    // Bind navbar links to scrollToContainer function
     $('#nav-my-projects').click(function() {
         scrollToContainer('projects-title');
     });
@@ -61,4 +54,26 @@ $(document).ready(function() {
     $('#nav-cv').click(function() {
         scrollToContainer('cv-cv');
     });
+});
+
+function copyToClipboard(text) {
+    // Create a temporary textarea element
+    const textarea = document.createElement('textarea');
+    textarea.value = text;
+    document.body.appendChild(textarea);
+    
+    // Select the text
+    textarea.select();
+    textarea.setSelectionRange(0, 99999); // For mobile devices
+
+    // Copy the text to the clipboard
+    document.execCommand('copy');
+    
+    // Remove the temporary textarea element
+    document.body.removeChild(textarea);
+}
+
+// Initialize Bootstrap tooltips
+$(document).ready(function(){
+    $('[data-toggle="tooltip"]').tooltip(); 
 });
